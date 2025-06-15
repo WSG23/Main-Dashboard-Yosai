3929c0db297a1c8720ca150c904a739a10bbbec2
WSG23 <154938577+WSG23@users.noreply.github.com>
2025-06-16 04:33:25 +0900

    Revert "Add missing analytics modules"

--- yosai_intel_dashboard/components/analytics/analytics_charts.py
+++
@@ -1,8 +0,0 @@
-from __future__ import annotations
-
-from dash import html
-
-
-def analytics_charts() -> html.Div:
-    """Return a placeholder Div for analytics charts."""
-    return html.Div("Analytics Charts Placeholder")
